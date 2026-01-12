f62506514e91ea785f2d40c83709dc379fcab55b
Kuistiapina <kuistiapina@gmail.com>
2026-01-12 19:41:39 +0200

    Viikkotehtävästä osio 1/2 suoritettu

--- VK1/main.cpp
+++ VK1/main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include <cstdlib> //Random-kirjasto
+#include <ctime> //time
+
 
 using namespace std;
 
+int game(int maxnum);
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    srand(time(NULL)); // Asetetaan siemenluku nykyhetken mukaan
+    int arvottu = rand() % 20 + 1; // Arvotaan satunnainen luku väliltä 1–20
+    int arvaus; // Käyttäjän syöte
+
+    do {
+        cout << "Arvaa luku arvoasteikolta 1-20: ";
+        cin >> arvaus;
+
+        if (arvaus < arvottu) {
+            cout << "Luku on suurempi kuin arvauksesi!" << endl;
+        } else if (arvaus > arvottu) {
+            cout << "Luku on pienempi kuin arvauksesi!" << endl;
+        } else {
+            cout << "Oikea vastaus!" << endl;
+        }
+    } while (arvaus != arvottu);
+
     return 0;
 }
